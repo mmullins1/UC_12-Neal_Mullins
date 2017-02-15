368b85118a1eb3ed31e1e0f924966f87e42cf938
Justin Neal <Justin Neal>
2017-02-15 13:39:16 -0800

    Base Files

---
+++ tests.js
@@ -0,0 +1,13 @@
+QUnit.test("Test the getAreaCode function.", function (assert) {
+    var num = "(415) 555-5555";
+    var result = getAreaCode(num);
+    assert.deepEqual(result, "415", "Valid area code test passed.");
+});
+QUnit.test("Errors thrown for getAreaCode", function (assert) {
+    assert.throws(function () {
+        getAreCode("415)444-5555");
+    }, "Missing '('. An error should have been thrown.");
+    assert.throws(function () {
+        getAreCode("(415 444-5555");
+    }, "Missing ')'. An error should have been thrown.");
+});
