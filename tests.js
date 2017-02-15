0e9fcc160a06ac47dfd520e670cdef36a306af1c
Justin Neal <Justin Neal>
2017-02-15 14:36:48 -0800

    Progress

--- tests.js
+++ tests.js
@@ -11,3 +11,8 @@ QUnit.test("Errors thrown for getAreaCode", function (assert) {
         getAreCode("(415 444-5555");
     }, "Missing ')'. An error should have been thrown.");
 });
+QUnit.test("Test the phone number validation function.", function (assert) {
+    var num = "(345) 846-8473";
+    var result = phoneValidate(num);
+    assert.deepEqual(result, "Valid number", "Validation test passed.");
+});
