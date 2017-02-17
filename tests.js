f0043386df0cb92a65cb3b7e27fe70001ab2022a
Maxfield B Mullins <mm61931@srcs.loc>
2017-02-17 13:18:15 -0800

    Tests

--- tests.js
+++ tests.js
@@ -5,9 +5,22 @@ QUnit.test("Test the getAreaCode function.", function (assert) {
 });
 QUnit.test("Errors thrown for getAreaCode", function (assert) {
     assert.throws(function () {
-        getAreCode("415)444-5555");
+        getAreaCode("415)444-5555");
     }, "Missing '('. An error should have been thrown.");
     assert.throws(function () {
-        getAreCode("(415 444-5555");
+        getAreaCode("(415 444-5555");
+    }, "Missing ')'. An error should have been thrown.");
+});
+QUnit.test("Test the getCO function.", function (assert) {
+    var num = "(415) 555-5555";
+    var result = getCO(num);
+    assert.deepEqual(result, "555", "Valid CO code test passed.");
+});
+QUnit.test("Errors thrown for getCO", function (assert) {
+    assert.throws(function () {
+        getCO("(415)4445555");
+    }, "Missing '-'. An error should have been thrown.");
+    assert.throws(function () {
+        getCO("(415 444-5555");
     }, "Missing ')'. An error should have been thrown.");
 });
