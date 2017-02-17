5b4b4b103d5aa9778635b19ebbf1832402c25655
Justin Neal <Justin Neal>
2017-02-17 13:16:40 -0800

    Added line code tests

--- tests.js
+++ tests.js
@@ -11,3 +11,16 @@ QUnit.test("Errors thrown for getAreaCode", function (assert) {
         getAreCode("(415 444-5555");
     }, "Missing ')'. An error should have been thrown.");
 });
+QUnit.test("Test the getLineCode function.", function (assert) {
+    var num = "(415) 555-5555";
+    var result = getLineCode(num);
+    assert.deepEqual(result, "5555", "Valid area code test passed.");
+});
+QUnit.test("Errors thrown for getLineCode", function (assert) {
+    assert.throws(function () {
+        getLineCode("(415)444-555");
+    }, "Missing digit. An error should have been thrown.");
+    assert.throws(function () {
+        getLineCode("(415 444-5e55");
+    }, "Incorrect input. An error should have been thrown.");
+});
